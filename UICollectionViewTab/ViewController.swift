4d0b1ac2726b90e0cc0e3413691c39bb3e054f9a
aya2453 <aya.nk.0116@gmail.com>
2020-09-15 20:16:18 +0900

    Initial Commit

---
+++ UICollectionViewTab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UICollectionViewTab
+//
+//  Created by 中川彩 on 2020/09/15.
+//  Copyright © 2020 aya2453. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
